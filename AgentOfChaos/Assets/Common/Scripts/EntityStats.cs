53c1a788858bb31d0182fd7cee1a16e14627bbc6
MrJellyB <shvachernoam@gmail.com>
2021-08-26 01:25:39 +0300

    added healthbar

---
+++ AgentOfChaos/Assets/Common/Scripts/EntityStats.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void EntityDeathHandler(EntityStats stats);
+
+public class EntityStats : MonoBehaviour
+{
+    private int originalHp;
+    private Transform ownHealthBar;
+
+    public int hp = 100;
+    public int hit = 10;
+    public bool showBar = false;
+    public Transform HealthBar;
+
+    public static event EntityDeathHandler EntityDeathEvent;
+
+    private void Start()
+    {
+        originalHp = hp;
+        if (showBar)
+        {
+            ownHealthBar = Instantiate(HealthBar, transform.position + (Vector3.up * 2), Quaternion.identity);
+        }
+    }
+
+    // TODO: change collsion to ballistic object class
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (hp - hit < 0)
+        {
+            EntityStats.EntityDeathEvent?.Invoke(this);
+            gameObject.SetActive(false);
+            ownHealthBar?.gameObject.SetActive(false);
+        } 
+        else
+        {
+            hp -= hit;
+
+            if (showBar)
+            {
+                Transform healthPlane = ownHealthBar.Find("healthPlane");
+                healthPlane.localScale = new Vector3(
+                    healthPlane.localScale.x * (float)hp / (float)originalHp, 
+                    healthPlane.localScale.y,
+                    healthPlane.localScale.z
+                );
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (showBar)
+        {
+            Vector3 updatedPos = transform.position + (Vector3.up * 2);
+            ownHealthBar.transform.SetPositionAndRotation(updatedPos, Quaternion.identity);
+        }
+    }
+}
